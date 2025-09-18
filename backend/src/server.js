f8e5cf0a8fc4437121b350b3e4d04f8fa9cf6c25
Brook07 <utsavadhr@gmail.com>
2025-09-18 21:51:15 +0545

    bla bla bla

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -10,5 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages",messageRoutes);
 
 app.listen(PORT, () => console.log("Server running on port : " + PORT ));
