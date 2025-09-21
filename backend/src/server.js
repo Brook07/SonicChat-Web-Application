7f9be8d7af6e5338755cf9fcfdbfca2e60a18836
Brook07 <utsavadhr@gmail.com>
2025-09-21 10:46:40 +0545

    engines field added in packages.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use("/api/messages",messageRoutes);
 
 //make ready for deployment
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path,join(_dirname, "../frontend/dist")))
+    app.use(express.static(path.join(_dirname, "../frontend/dist")))
 
     app.get("*", (_,res) => {
       res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))  
