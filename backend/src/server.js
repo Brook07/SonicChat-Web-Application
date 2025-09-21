9dffa91bc9521fb1045cc23715439df93cb773c2
Brook07 <utsavadhr@gmail.com>
2025-09-21 10:43:19 +0545

    engines field added in packages.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,10 +20,8 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path,join(_dirname, "../frontend/dist")))
 
     app.get("*", (_,res) => {
-      res.sendFile(path.join(_dirname, "../forntend","dist","index.html"))  
+      res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))  
     })
 }
 
-//
-
-app.listen(PORT, () => console.log("Server running on port : " + PORT ));
+app.listen(PORT, () => console.log("Server running on port : " + PORT ));                    
